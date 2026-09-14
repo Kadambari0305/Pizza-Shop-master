3c58b843f28489705117518d087500b642e1134c
Kadambari0305 <kadamabrimarne1@gmail.com>
2026-09-14 17:31:15 +0530

    Update default MONGODB_URL to user cluster

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,6 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL:
     process.env.MONGODB_URL ||
-    "mongodb+srv://abdul:12345@pizashopdb-gadyg.mongodb.net/PizaShopDB?retryWrites=true&w=majority",
+    "mongodb+srv://PICTPBL1:deepanil@cluster0.43ytz.mongodb.net/PizaShopDB?retryWrites=true&w=majority",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
 };
